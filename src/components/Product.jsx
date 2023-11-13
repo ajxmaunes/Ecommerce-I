2338edd3a37250f072525e33b0c028dda5e032ce
ajxmaunes <aljunemaunes0@gmail.com>
2023-11-13 17:54:17 +0800

    change some function

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -13,12 +13,12 @@ const Product = ({product}) => {
   return (
     <div>
       <div className='border border-[#e4e4e4] hover:border-2 hover:border-gray-600 h-[300px] mb-2 relative overflow-hidden group transition'>
-        <div className='w-full h-full flex justify-center items-center'>
+        <Link onClick={() => window.scrollTo(0, 0)} to={`/product/${id}`} className='w-full h-full flex justify-center items-center cursor-pointer'>
           {/* image */}
           <div className='w-[200px] mx-auto flex justify-center items-center'>
             <img className='max-h-[160px] group-hover:scale-110 transition duration-300' src={image} alt="" />
           </div>
-        </div>
+        </Link>
         {/* buttons */}
         <div className='absolute top-6 -right-11 group-hover:right-5 p-2 flex flex-col items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300'>
           <button onClick={() => addToCart(product, id)}>
